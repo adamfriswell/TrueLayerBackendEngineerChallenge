21a0d7e1e086614807e94e158c7cee2545c71a95
Adam Friswell <adamfriswell@googlemail.com>
2022-05-05 21:13:26 +0100

    add empty test class

---
+++ Tests/PokemonControllerTests.cs
@@ -0,0 +1,17 @@
+using System;
+using Xunit;
+
+namespace TrueLayerBackendEngineerChallenge.Tests
+{
+    public class PokemonControllerTests
+    {
+       [Fact]
+        public void PokemonController_Get_Success(){
+            //Arrange
+            //Act
+            //Assert
+            return;
+        }
+  
+    }
+}
