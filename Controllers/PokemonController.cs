df6fc8ba6c7f805abd99854b5d7380e84008c600
Adam Friswell <adamfriswell@googlemail.com>
2022-05-05 21:04:12 +0100

    remove boilerplate weather forecast class and rename controller

---
+++ Controllers/PokemonController.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+
+namespace TrueLayerBackendEngineerChallenge.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class PokemonController : ControllerBase
+    {
+
+        private readonly ILogger<PokemonController> _logger;
+
+        public PokemonController(ILogger<PokemonController> logger)
+        {
+            _logger = logger;
+        }
+
+        [HttpGet]
+        public void Get()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
